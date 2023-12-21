599bd223816efadfbd418a3e2b743e9fceccd389
Vinicius Luna <vini.viniciusluna@me.com>
2023-12-21 18:29:14 -0300

    prdutos migrados

--- models/produtos.py
+++ models/produtos.py
@@ -1,6 +1,8 @@
+import sqlite3
 class Produtos:
     
-    def __init__(self, nome="", preco=0, quantidade=0):
+    def __init__(self, id = 0, nome="", preco=0, quantidade=0):
+        self.id = id
         self.nome = nome
         self.preco = preco
         self.quantidade = quantidade
@@ -8,8 +10,22 @@ class Produtos:
     produtos = []
 
     def incluir(self):
-        Produtos.produtos.append(self)
+        conn = sqlite3.connect('db/cadastro_de_produtos.db')
+        cursor = conn.cursor()
+        #Insert no sqlite
+        cursor.execute('INSERT INTO produtos (id, nome, preco, quantidade) VALUES (?, ?, ?)', (self.nome, self.preco, self.quantidade))
+
+        conn.commit()
+        conn.close
     
     @staticmethod
     def todos():
-        return Produtos.produtos
+        #select no sqlite
+        conn = sqlite3.connect('db/cadastro_de_produtos.db')
+        cursor = conn.cursor
+        cursor.execute('SELECT * from produtos')
+        produtos_db = cursor.fetchall()
+        conn.close
+        #precisa retornar os dados tipados com obj produto
+        
+        return [Produtos(id=id, nome=nome, preco=preco, quantidade=quantidade) for id, nome, preco, quantidade in produtos_db]
